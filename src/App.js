a2db028135d3d47cb555c4dc2899b5bc6aa89ade
martodmih <mt20180064@student.fon.bg.ac.rs>
2023-05-09 02:11:21 +0200

    dodata jos jedna stranica

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import NavBar from './komponente/NavBar';
 import SviTurniri from './komponente/SviTurniri';
-
+import MojiTurniri from './komponente/MojiTurniri';
 
 function App() {
   return (
     <BrowserRouter className="App">
     <Routes>
     <Route path="/" element = {<NavBar />}>;
-    <Route path="mojiTurniri"></Route>;
+    <Route path="mojiTurniri" element ={<MojiTurniri/>}></Route>;
     <Route path="sviTurniri" element ={<SviTurniri/>}></Route>
     </Route>
     </Routes>
